ddae83449356a2928ec165b12c47541ec4761d7d
João Paulo (Jp) <115497022+Joaolima2003@users.noreply.github.com>
2023-06-05 00:48:14 -0300

    Update usuario_db.py

--- functions/usuario_db.py
+++ functions/usuario_db.py
@@ -17,12 +17,8 @@ def login(conexao,email,senha):
     ])
     return cursor.fetchall()
 
-def usuario_atualizar(conexao,usuario_novo,id):
-    sql = 'UPDATE usuarios SET usuario_name=? WHERE usuario_id=?'
-    cursor = conexao.cursor()
-    cursor.execute(sql, [usuario_novo, id])
-    conexao.commit()
 
+  
 
 
    
